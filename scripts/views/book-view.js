0f6b0870916225b5a708308028b664654cd5303a
Mario Flores Jr <m.flores4821@gmail.com>
2018-03-01 13:48:59 -0800

    view work

--- scripts/views/book-view.js
+++ scripts/views/book-view.js
@@ -3,43 +3,48 @@
 var app = app || {};
 
 (function(module) {
-const bookView = {};
 
-bookView.initIndexPage = function() {
-    $('.error-view-container').hide();
-    $('.book-details').hide();
-    $('#book-form').hide();
+  function resetView() {
+    $('.container').hide();
+  }
+
+  const bookView = {};
+
+  bookView.initIndexPage = function() {
+    resetView();
     $('.book-view').show();
     module.Book.all.map(book => $('#book-list').append(book.toHtml()));
-}
+  };
 
-bookView.initDetailPage = function(ctx) {
-  $('.book-view').show();
-  $('.book-details').empty();
-  // $('.error-view-container').hide();
-  // $('#book-form').hide();
- let template = Handlebars.compile($('#book-details').text());
- $('.book-details').append(template(ctx));
-}
+  bookView.initDetailPage = function(ctx) {
+    resetView();
+    $('.book-details').show();
+    $('.book-details').empty();
+    // $('.error-view-container').hide();
+    // $('#book-form').hide();
+    let template = Handlebars.compile($('#book-detail-template').text());
+    $('.book-details').append(template(ctx));
+  };
 
-bookView.initCreateFormPage= function() {
-$('#book-form').show();
-$('#create-form').on('submit', function(event) {
-event.preventDefault();
+  bookView.initCreateFormPage = function() {
+    resetView();
+    $('#book-form').show();
+    $('#create-form').on('submit', function(event) {
+      event.preventDefault();
 
-let book = {
-  title: event.target.title.value,
-  author: event.target.author.value,
-  isbn: event.target.isbn.value,
-  image_url: event.target.image_url.value,
-  description: event.target.description.value,  
-}
-})
-}
+      let book = {
+        title: event.target.title.value,
+        author: event.target.author.value,
+        isbn: event.target.isbn.value,
+        image_url: event.target.image_url.value,
+        description: event.target.description.value,
+      };
+    });
+  };
 
-module.bookView = bookView;
-})(app)
+  module.bookView = bookView;
+})(app);
 
 $(function() {
-    app.Book.fetchAll(app.bookView.initIndexPage);
-})
+  app.Book.fetchAll(app.bookView.initIndexPage);
+});
